899d291b2de17df8706ff5260d3a96d6f54a1329
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-05-01 21:40:03 +0800

    Add login route

--- backend/app.py
+++ backend/app.py
@@ -20,3 +20,26 @@ db = mysql.connector.connect(
     database="CodeCrypt" 
 )
 cursor = db.cursor()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		identifier = request.form['identifier']
+		password = request.form['password']
+
+		cursor.execute("SELECT * FROM users WHERE email = %s OR username = %s", (identifier, identifier))
+		user = cursor.fetchone()
+
+		if user:
+			stored_hashed_password = user[4]
+			if check_password_hash(stored_hashed_password, password):
+				session['user_id'] = user[0]  
+				session['username'] = user[3]  
+                
+				return redirect(url_for('homepage'))
+			else:
+				flash('Invalid email/username or password', 'danger')
+		else:
+			flash('Invalid email/username or password', 'danger')
+
+	return render_template('login.html')
